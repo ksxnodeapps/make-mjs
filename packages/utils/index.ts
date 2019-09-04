131b327459a1252620145df831172c2e0c13cb56
khai96_ <hvksmr1996@gmail.com>
2019-09-04 14:49:15 +0700

    Create objectExtends and addProperty functions

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,3 +1,7 @@
+import { Assign } from 'utility-types'
+
+type NullObject<X> = X extends null ? {} : X
+
 export type MaybePromise<X> = X | Promise<X>
 
 export const REJECTION_SILENCER = (): void => undefined
@@ -29,4 +33,60 @@ export function joinPath (...paths: string[]) {
   return paths
     .map(removeTrailingSeparator)
     .join('/')
+}
+
+type ObjectExtends<
+  Proto extends object | null,
+  Properties extends object | null
+> = Assign<
+  NullObject<Proto>,
+  NullObject<Properties>
+>
+
+/**
+ * Create an object with `proto` as prototype
+ * and `properties` as own properties
+ * @param proto Prototype to extends upon
+ * @param properties Properties to add
+ */
+export function objectExtends<
+  Proto extends object | null,
+  Properties extends object | null
+> (
+  proto: Proto,
+  properties: Properties
+): ObjectExtends<Proto, Properties> {
+  return Object.assign(
+    Object.create(proto),
+    properties
+  )
+}
+
+type AddProperty<
+  Proto extends object | null,
+  Key extends string | number | symbol,
+  Value
+> = ObjectExtends<Proto, {
+  [key in Key]: Value
+}>
+
+/**
+ * Create an object with `proto` as prototype
+ * and `[key]: value` as the only own properties
+ * @param proto Prototype to extends upon
+ * @param key Property key
+ * @param value Property value
+ */
+export function addProperty<
+  Proto extends object | null,
+  Key extends string | number | symbol,
+  Value
+> (
+  proto: Proto,
+  key: Key,
+  value: Value
+): AddProperty<Proto, Key, Value> {
+  const object = Object.create(proto)
+  object[key] = value
+  return object
 }
