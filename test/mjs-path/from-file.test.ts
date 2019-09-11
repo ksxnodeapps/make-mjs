49668b8b5b16018e19538694dc17be096345d0ff
khai96_ <hvksmr1996@gmail.com>
2019-09-11 10:58:07 +0700

    Update from-file.test.ts

--- test/mjs-path/from-file.test.ts
+++ test/mjs-path/from-file.test.ts
@@ -1,15 +1,81 @@
-import { fromFile } from '@make-mjs/mjs-path'
+import { fromFileWithoutChecking, fromFile } from '@make-mjs/mjs-path'
 
-const get = (modulePath: string) => fromFile({ modulePath })
+import {
+  appendFile,
+  fillAllMockedArrays
+} from './fsx-mocks'
 
-it('without extension', () => {
-  expect(get('./foo/bar')).toBe('./foo/bar.mjs')
+describe.skip('fromFileWithoutChecking', () => {
+  const get = (modulePath: string) => fromFileWithoutChecking({ modulePath })
+
+  it('without extension', () => {
+    expect(get('./foo/bar')).toBe('./foo/bar.mjs')
+  })
+
+  it('with ".js" extension', () => {
+    expect(get('./foo/bar.js')).toBe('./foo/bar.mjs')
+  })
+
+  it('with other extension', () => {
+    expect(get('./foo/bar.css')).toBe('./foo/bar.css')
+  })
 })
 
-it('with ".js" extension', () => {
-  expect(get('./foo/bar.js')).toBe('./foo/bar.mjs')
-})
+describe('fromFile', () => {
+  afterEach(fillAllMockedArrays)
 
-it('with other extension', () => {
-  expect(get('./foo/bar.css')).toBe('./foo/bar.css')
+  describe('when forceMjs is false and .mjs files exist', () => {
+    const get = (modulePath: string) =>
+      fromFile({ modulePath, forceMjs: false, moduleContainer: '' })
+
+    it('without extension', async () => {
+      appendFile(['./foo/bar.mjs'])
+      expect(await get('./foo/bar')).toBe('./foo/bar.mjs')
+    })
+
+    it('with ".js" extension', async () => {
+      appendFile(['./foo/bar.mjs'])
+      expect(await get('./foo/bar.js')).toBe('./foo/bar.mjs')
+    })
+
+    it('with other extension', async () => {
+      expect(await get('./foo/bar.css')).toBe('./foo/bar.css')
+    })
+  })
+
+  describe('when forceMjs is false and .mjs files do not exist', () => {
+    const get = (modulePath: string) =>
+      fromFile({ modulePath, forceMjs: false, moduleContainer: '' })
+
+    it('without extension', async () => {
+      appendFile(['./foo/bar.mjs'])
+      expect(await get('./foo/bar')).toBe('./foo/bar.mjs')
+    })
+
+    it('with ".js" extension', async () => {
+      appendFile(['./foo/bar.mjs'])
+      expect(await get('./foo/bar.js')).toBe('./foo/bar.mjs')
+    })
+
+    it('with other extension', async () => {
+      expect(await get('./foo/bar.css')).toBe('./foo/bar.css')
+    })
+  })
+
+  describe('when forceMjs is true', () => {
+    const get = (modulePath: string) =>
+      fromFile({ modulePath, forceMjs: true, moduleContainer: '' })
+
+    it('without extension', async () => {
+      expect(await get('./foo/bar')).toBe('./foo/bar.mjs')
+    })
+
+    it('with ".js" extension', async () => {
+      expect(await get('./foo/bar.js')).toBe('./foo/bar.mjs')
+    })
+
+    it('with other extension', async () => {
+      expect(await get('./foo/bar.css')).toBe('./foo/bar.css')
+    })
+  })
 })
