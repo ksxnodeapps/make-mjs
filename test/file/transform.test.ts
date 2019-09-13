b667ef3de8054f544fa09cde5cd94986a8f73e01
khai96_ <hvksmr1996@gmail.com>
2019-09-13 17:42:32 +0700

    Add test for new change

--- test/file/transform.test.ts
+++ test/file/transform.test.ts
@@ -181,3 +181,201 @@ describe('without isMjsPackage', () => {
     expect(formatCode(result.code)).toBe(expectedCode)
   })
 })
+
+describe('with isMjsPackage always return false', () => {
+  const code = `
+    import './internal-file'
+    import './internal-dir'
+
+    import 'nonmjs-external'
+    import 'nonmjs-external/dir-without-manifest'
+    import 'nonmjs-external/dir-with-manifest'
+    import 'nonmjs-external/file'
+
+    import 'external-default/index.mjs'
+    import 'external-default/dir-without-manifest'
+    import 'external-default/dir-with-manifest'
+    import 'external-default/file'
+
+    import 'external-main'
+    import 'external-main/dir-without-manifest'
+    import 'external-main/dir-with-manifest'
+    import 'external-main/file'
+
+    import 'external-browser'
+    import 'external-browser/dir-without-manifest'
+    import 'external-browser/dir-with-manifest'
+    import 'external-browser/file'
+
+    import 'external-module'
+    import 'external-module/dir-without-manifest'
+    import 'external-module/dir-with-manifest'
+    import 'external-module/file'
+
+    export * from './internal-file'
+    export * from 'external-module'
+    export { default } from './internal-dir'
+
+    export async function main () {
+      const a = await import('./internal-file')
+      const b = await import('external-module')
+      return [a, b]
+    }
+  `
+
+  function get () {
+    return transformCode(code, {
+      moduleContainer: 'node_modules',
+      isMjsPackage: () => false
+    })
+  }
+
+  it('matches snapshot', async () => {
+    const result = await get()
+    expect(formatCode(result.code)).toMatchSnapshot()
+  })
+
+  it('returns expected code', async () => {
+    const expectedCode = formatCode(`
+      import './internal-file.mjs'
+      import './internal-dir/index.mjs'
+
+      import 'nonmjs-external'
+      import 'nonmjs-external/dir-without-manifest'
+      import 'nonmjs-external/dir-with-manifest'
+      import 'nonmjs-external/file'
+
+      import 'external-default/index.mjs'
+      import 'external-default/dir-without-manifest'
+      import 'external-default/dir-with-manifest'
+      import 'external-default/file.mjs'
+
+      import 'external-main/main-entry.mjs'
+      import 'external-main/dir-without-manifest'
+      import 'external-main/dir-with-manifest/dir-main-entry.mjs'
+      import 'external-main/file.mjs'
+
+      import 'external-browser/browser-entry'
+      import 'external-browser/dir-without-manifest'
+      import 'external-browser/dir-with-manifest/dir-browser-entry'
+      import 'external-browser/file.mjs'
+
+      import 'external-module/module-entry'
+      import 'external-module/dir-without-manifest'
+      import 'external-module/dir-with-manifest/dir-module-entry'
+      import 'external-module/file.mjs'
+
+      export * from './internal-file.mjs'
+      export * from 'external-module/module-entry'
+      export { default } from './internal-dir/index.mjs'
+
+      export async function main () {
+        const a = await import('./internal-file.mjs')
+        const b = await import('external-module/module-entry')
+        return [a, b]
+      }
+    `)
+
+    const result = await get()
+    expect(formatCode(result.code)).toBe(expectedCode)
+  })
+})
+
+describe('with isMjsPackage always return true', () => {
+  const code = `
+    import './internal-file'
+    import './internal-dir'
+
+    import 'nonmjs-external'
+    import 'nonmjs-external/dir-without-manifest'
+    import 'nonmjs-external/dir-with-manifest'
+    import 'nonmjs-external/file'
+
+    import 'external-default/index.mjs'
+    import 'external-default/dir-without-manifest'
+    import 'external-default/dir-with-manifest'
+    import 'external-default/file'
+
+    import 'external-main'
+    import 'external-main/dir-without-manifest'
+    import 'external-main/dir-with-manifest'
+    import 'external-main/file'
+
+    import 'external-browser'
+    import 'external-browser/dir-without-manifest'
+    import 'external-browser/dir-with-manifest'
+    import 'external-browser/file'
+
+    import 'external-module'
+    import 'external-module/dir-without-manifest'
+    import 'external-module/dir-with-manifest'
+    import 'external-module/file'
+
+    export * from './internal-file'
+    export * from 'external-module'
+    export { default } from './internal-dir'
+
+    export async function main () {
+      const a = await import('./internal-file')
+      const b = await import('external-module')
+      return [a, b]
+    }
+  `
+
+  function get () {
+    return transformCode(code, {
+      moduleContainer: 'node_modules',
+      isMjsPackage: () => true
+    })
+  }
+
+  it('matches snapshot', async () => {
+    const result = await get()
+    expect(formatCode(result.code)).toMatchSnapshot()
+  })
+
+  it('returns expected code', async () => {
+    const expectedCode = formatCode(`
+      import './internal-file.mjs'
+      import './internal-dir/index.mjs'
+
+      import 'nonmjs-external/index.mjs'
+      import 'nonmjs-external/dir-without-manifest/index.mjs'
+      import 'nonmjs-external/dir-with-manifest/index.mjs'
+      import 'nonmjs-external/file.mjs'
+
+      import 'external-default/index.mjs'
+      import 'external-default/dir-without-manifest/index.mjs'
+      import 'external-default/dir-with-manifest/index.mjs'
+      import 'external-default/file.mjs'
+
+      import 'external-main/main-entry.mjs'
+      import 'external-main/dir-without-manifest/index.mjs'
+      import 'external-main/dir-with-manifest/dir-main-entry.mjs'
+      import 'external-main/file.mjs'
+
+      import 'external-browser/browser-entry'
+      import 'external-browser/dir-without-manifest/index.mjs'
+      import 'external-browser/dir-with-manifest/dir-browser-entry'
+      import 'external-browser/file.mjs'
+
+      import 'external-module/module-entry'
+      import 'external-module/dir-without-manifest/index.mjs'
+      import 'external-module/dir-with-manifest/dir-module-entry'
+      import 'external-module/file.mjs'
+
+      export * from './internal-file.mjs'
+      export * from 'external-module/module-entry'
+      export { default } from './internal-dir/index.mjs'
+
+      export async function main () {
+        const a = await import('./internal-file.mjs')
+        const b = await import('external-module/module-entry')
+        return [a, b]
+      }
+    `)
+
+    const result = await get()
+    expect(formatCode(result.code)).toBe(expectedCode)
+  })
+})
