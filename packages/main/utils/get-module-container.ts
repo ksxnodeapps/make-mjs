f3cc878e326c4c975832d89a7898bf12387c9a00
khai96_ <hvksmr1996@gmail.com>
2019-09-16 14:29:32 +0700

    Fix a bug that uses wrong node_modules paths

--- packages/main/utils/get-module-container.ts
+++ packages/main/utils/get-module-container.ts
@@ -1,8 +1,9 @@
-import { iterateAncestorDirectories, joinPath } from '@make-mjs/utils'
+import { join } from 'path' // always use path.join when we are not dealing with module urls
+import iterateAncestorDirectories from 'ancestor-directories'
 
 export function * getModuleContainer (path: string, basename = 'node_modules') {
   for (const dir of iterateAncestorDirectories(path)) {
-    yield joinPath(dir, basename)
+    yield join(dir, basename)
   }
 }
 
