838dd12cdb248cca0ca05eb9f3d6ddaa6a01b9cb
khai96_ <hvksmr1996@gmail.com>
2019-09-26 18:24:38 +0700

    Updates

--- make-mjs/assets/js/search.js
+++ make-mjs/assets/js/search.js
@@ -1,3 +1,3 @@
 var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"interfaces/process.html#exit.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"Process.exit"},{"id":1,"kind":65536,"name":"__type","url":"interfaces/console.html#info.__type-1","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"Console.info"},{"id":2,"kind":65536,"name":"__type","url":"interfaces/console.html#error.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"Console.error"}]};
+            typedoc.search.data = {"kinds":{},"rows":[]};
